9fc8f92b2411f9b238d420e788d177c88356bec1
Bruno Baffini <bruno.baffini@quintoandar.com.br>
2022-11-24 15:22:19 -0300

    WIP Select

--- src/components/Matches/index.js
+++ src/components/Matches/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import useForm from '../Form/useForm';
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Select from './StyledComponents';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { Place, SportsSoccer, CalendarToday, SignalCellularAltIcon } from '@mui/icons-material';
+import Input from '@mui/material/Input';
+import Button from '@mui/material/Button';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+//import Box from '../Form/StyledComponents';
+//import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { Place, SportsSoccer, CalendarToday, SignalCellularAlt } from '@mui/icons-material';
 
 const Matches = () => {
   const endereco = useForm();
+  const esporte = useForm();
+  const intensidade = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -22,31 +28,65 @@ const Matches = () => {
   return (
     <form onSubmit={handleSubmit}>
       <Box>
-        <Place />
-        <Input
-          label="Endereço"
-          type="text"
-        />
+        <Box flex>
+          <InputLabel id="localizacaoLabel">Localização</InputLabel>
+          <Place fontSize="large"/>
+          <Input
+            labelId="localizacaoLabel"
+            id="localizacao"
+            type="text"
+          />
+        </Box>
+
+        <Box flex>
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="esporteLabel">Esporte</InputLabel>
+            <SportsSoccer fontSize="large"/>
+            <Select
+              labelId="esporteLabel"
+              id="esporte"
+              value={esporte.value}
+              label="Esporte"
+              onChange={esporte.onChange}
+              displayEmpty
+            >
+              <MenuItem value={''}>Selecione o esporte</MenuItem>
+              <MenuItem value={'Futebol'}>Futebol</MenuItem>
+              <MenuItem value={'Basquete'}>Basquete</MenuItem>
+              <MenuItem value={'Volei'}>Volei</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Box flex>
+          <InputLabel id="dataLabel">Data</InputLabel>
+          <CalendarToday fontSize="large"/>
+          {/* <DatePicker
+            labelId="dataLabel"
+          /> */}
+        </Box>
+
+        <Box flex>
+          <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="intensidadeLabel">Intensidade</InputLabel>
+            <SignalCellularAlt fontSize="large"/>
+            <Select
+              labelId="intensidadeLabel"
+              id="intensidade"
+              value={intensidade.value}
+              label="Intensidade"
+              onChange={intensidade.onChange}
+              displayEmpty
+            >
+              <MenuItem value={''}>Selecione a intensidade</MenuItem>
+              <MenuItem value={'Iniciante'}>Iniciante</MenuItem>
+              <MenuItem value={'Intermediário'}>Intermediário</MenuItem>
+              <MenuItem value={'Experiente'}>Experiente</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Button>Criar</Button>
       </Box>
-      <Box>
-        <SportsSoccer />
-        <Select
-          label="Esporte"
-        />
-      </Box>
-      <Box>
-        <CalendarToday />
-        <DatePicker
-          label="Data"
-        />
-      </Box>
-      <Box>
-        <SignalCellularAltIcon />
-        <Select
-          label="Intensidade"
-        />
-      </Box>
-      <Button>Criar</Button>
     </form>
   );
 };
